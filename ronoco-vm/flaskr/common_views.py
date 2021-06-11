535af5f9148a4a26eca23f0345972c21be3f7c31
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2021-06-11 15:26:32 +0200

    feat(ronoco-vm): add robot_state on common endpoint

--- ronoco-vm/flaskr/common_views.py
+++ ronoco-vm/flaskr/common_views.py
@@ -1,10 +1,39 @@
-from flask import (
-    Blueprint
-)
+from flask import Blueprint, request
+import rospy
+from roscpp.srv import GetLoggers
+from werkzeug.exceptions import NotFound
 
 bp = Blueprint('common_views', __name__, url_prefix='/')
 
 
-@bp.route('/', methods=('GET', 'POST'))
+@bp.route('/', methods=['GET'])
 def index():
+    """
+    GET Method
+
+    TODO : change index behaviour
+    :return: if everything is ok : 'Hello World !' else an HttpError
+    """
     return 'Hello World !'
+
+
+@bp.route('/robot_state', methods=['GET'])
+def robot_state():
+    """
+    GET Method
+
+    Check if you can communicate with a ros master
+        + Use rosservice /rosout/get_loggers
+        + Node: /rosout
+        + Type: roscpp/GetLoggers
+        + Args:
+
+    :return: {'robot_state': True} if communication with rosmaster is possible, NotFound exception else
+    """
+    if request.method == 'GET':
+        get_loggers = rospy.ServiceProxy('rosout/get_loggers', GetLoggers)
+        try:
+            get_loggers()
+        except rospy.service.ServiceException:
+            raise NotFound()
+        return {'robot_state': True}
