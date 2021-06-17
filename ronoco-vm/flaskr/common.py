96d92101225471e9b3713dd74389a9a3c2a18717
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2021-06-17 14:48:14 +0200

    feat(ronoco-vm): add docstring

--- ronoco-vm/flaskr/common.py
+++ ronoco-vm/flaskr/common.py
@@ -1,36 +1,47 @@
-from flask import Blueprint, request
-import rospy
-from roscpp.srv import GetLoggers
+"""
+This file implements the common endpoint
+"""
+from flask import Blueprint
 from werkzeug.exceptions import NotFound
 
-bp = Blueprint('common_views', __name__, url_prefix='/')
+import rospy
+from roscpp.srv import GetLoggers
 
 
-@bp.route('/', methods=['GET'])
-def index():
+class Common:
     """
-    GET Method
-
-    TODO : change index behaviour
-    :return: if everything is ok : 'Hello World !' else an HttpError
+    Definition of common endpoint
     """
-    return 'Hello World from everyone !'
+    def __init__(self):
+        self.bp = Blueprint('common_views', __name__, url_prefix='/')
 
+        self.bp.route('/', methods=['GET'])(self.index)
+        self.bp.route('/robot_state')(self.robot_state)
 
-@bp.route('/robot_state')
-def robot_state():
-    """
-    Check if you can communicate with a ros master
-        + Use rosservice /rosout/get_loggers
-        + Node: /rosout
-        + Type: roscpp/GetLoggers
-        + Args:
+    @staticmethod
+    def index():
+        """
+        GET Method
 
-    :return: {'robot_state': True} if communication with rosmaster is possible, NotFound exception else
-    """
-    get_loggers = rospy.ServiceProxy('rosout/get_loggers', GetLoggers)
-    try:
-        get_loggers()
-    except rospy.service.ServiceException:
-        raise NotFound()
-    return {'robot_state': True}
+        TODO : change index behaviour
+        :return: if everything is ok : 'Hello World !' else an HttpError
+        """
+        return 'Hello World from everyone !'
+
+    @staticmethod
+    def robot_state():
+        """
+        Check if you can communicate with a ros master
+            + Use rosservice /rosout/get_loggers
+            + Node: /rosout
+            + Type: roscpp/GetLoggers
+            + Args:
+
+        :return: {'robot_state': True} if communication with rosmaster is possible, NotFound exception else
+        """
+        get_loggers = rospy.ServiceProxy('rosout/get_loggers', GetLoggers)
+        try:
+            get_loggers()
+        except rospy.service.ServiceException:
+            raise NotFound()
+        return {'robot_state': True}
--- ronoco-vm/flaskr/run.py
+++ ronoco-vm/flaskr/run.py
@@ -1,19 +1,30 @@
+"""
+This file is the entry point of flask api. Launch python3 flaskr/run.py to run flask server
+
+WIP : shutdown app when KeyboardInterrupt
+"""
 import os
-import subprocess
 import time
 
 from flask import Flask
 from flask_cors import CORS
 from flask_socketio import SocketIO
-from flaskr import topic_callback
 
 import rospy
+from flaskr import topic_callback
 from visualization_msgs.msg import InteractiveMarkerUpdate
 
 
 class ronoco_vm:
+    """
+    Define and setup flask server and ros topic suscriber / publisher for ronoco-vm
+    """
     app = None
+
     def __init__(self):
+        """
+        Launch flask server when ronoco_vm is created (this constructor uses SocketIO)
+        """
         self.create_app()
         socketio = SocketIO(self.app)
         rospy.init_node('user')
@@ -24,6 +35,11 @@ class ronoco_vm:
         rospy.spin()
 
     def create_app(self, test_config=None):
+        """
+        Build a Flask instance and configure it
+        :param test_config: path to configuration file (Défault : None)
+        :return: a Flask instance
+        """
         # create and configure the app
         self.app = Flask(__name__, instance_relative_config=True)
         self.app.config.from_mapping(
@@ -45,8 +61,14 @@ class ronoco_vm:
         self.setup_app()
 
     def setup_app(self):
+        """
+        Register blueprint in app.
+
+        The class attribute "app" must contain an Flask instance
+        :return: None
+        """
         from flaskr import common
-        self.app.register_blueprint(common.bp)
+        self.app.register_blueprint(common.Common().bp)
 
         from flaskr import free
         self.app.register_blueprint(free.bp)
@@ -56,7 +78,11 @@ class ronoco_vm:
         CORS(self.app)
 
     def subscribe_topic(self):
-        response = rospy.Subscriber(
+        """
+        Uses rospy to subscribe to the different topics needed by the API
+        :return: None
+        """
+        rospy.Subscriber(
             "/rviz_moveit_motion_planning_display/robot_interaction_interactive_marker_topic/update",
             InteractiveMarkerUpdate, topic_callback.position_callback)
         time.sleep(0.5)
