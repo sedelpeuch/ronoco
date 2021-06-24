db36ee03b6367233d12dd6002bb8c671fe974a5d
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2021-06-24 15:41:36 +0200

    feat(ronoco-ui): form treatment

--- ronoco-ui/src/point.js
+++ ronoco-ui/src/point.js
@@ -6,11 +6,13 @@ async function recordPositionRviz() {
     let result = await post("http://127.0.0.1:5000/point/add/rviz", {})
     console.log(result)
 }
+document.getElementById('rviz').addEventListener('click',recordPositionRviz)
 
 async function recordPositionFree() {
     let result = await post("http://127.0.0.1:5000/point/add/free", {})
     console.log(result)
 }
+document.getElementById('free').addEventListener('click',recordPositionFree)
 
 async function getPosition() {
     let result = await get("http://127.0.0.1:5000/point/get")
@@ -23,3 +25,17 @@ async function deletePosition() {
     console.log(result)
 }
 document.getElementById('DeletePosition').addEventListener('click',deletePosition)
+
+async function getPositionId(){
+    let id = document.getElementById("id").value;
+    let result = await get("http://127.0.0.1:5000/point/get/"+id)
+    console.log(result)
+}
+document.getElementById('GetPositionId').addEventListener('click',getPositionId)
+
+async function deletePositionId(){
+    let id = document.getElementById("id").value;
+    let result = await post("http://127.0.0.1:5000/point/delete/"+id, {})
+    console.log(result)
+}
+document.getElementById('DeletePositionId').addEventListener('click',deletePositionId)
