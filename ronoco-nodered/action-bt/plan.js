36dfead508558463709fba773b7e429a824dbfd1
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2021-07-01 16:02:01 +0200

    feat(ronoco-nodered): add action-bt nodes

---
+++ ronoco-nodered/action-bt/plan.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = function (RED) {
+    function Plan(config) {
+        RED.nodes.createNode(this, config);
+        const node = this;
+        node.on('input', function (msg) {
+            node.send(msg);
+        });
+    }
+
+    RED.nodes.registerType("plan", Plan);
+}
