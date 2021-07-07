962332fca0ef6477574a2abb51b90a2b46e0fab4
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2021-07-07 16:05:23 +0200

    ♻ refactor(ronoco-nodered): standardization of data fields

--- ronoco-nodered/native-bt/root.js
+++ ronoco-nodered/native-bt/root.js
@@ -4,6 +4,7 @@ module.exports = function (RED) {
     function Root(config) {
         RED.nodes.createNode(this, config);
         const node = this;
+        node.warn("Behavior tree successfully recorded")
         node.on('input', function (msg) {
             node.send(msg);
         });
