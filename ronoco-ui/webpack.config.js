27c5310054d52387881256f3a491cde15700a7b7
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2021-08-30 17:16:10 +0200

    ✨ feat(ronoco-ui): update package and webpack

--- ronoco-ui/webpack.config.js
+++ ronoco-ui/webpack.config.js
@@ -15,30 +15,6 @@ module.exports = {
         path: path.resolve(__dirname, "dist")
     },
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env"]
-                    }
-                }
-            },
-                  {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-        {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-        ]
-    },
-    
+
+    }
 };
