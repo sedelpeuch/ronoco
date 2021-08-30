b44f47a2fe5f42493fa917d95943ceba66da8318
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2021-08-30 17:15:20 +0200

    ✨ feat(ronoco): update launch file

--- launch/launch.py
+++ launch/launch.py
@@ -7,8 +7,13 @@ from pathlib import Path
 import rospy
 
 ronoco_path = rospy.get_param("ronoco_path")
-node_red = subprocess.Popen("node-red")
+editor = rospy.get_param("editor")
+if editor == "nodered":
+    node_red = subprocess.Popen("node-red")
+elif editor == "blockly":
+    ronoco_blockly = subprocess.Popen(shlex.split("npm start"), cwd=ronoco_path + "/ronoco-blockly/")
 ronoco_ui = subprocess.Popen(shlex.split("npm start"), cwd=ronoco_path + "/ronoco-ui/")
 ronoco_vm = os.system("cd " + ronoco_path + "/ronoco-vm/ && python3 ronoco_vm/run.py")
 node_red.kill()
 subprocess.Popen(shlex.split("npm stop"), cwd = ronoco_path + "/ronoco-ui/")
+subprocess.Popen(shlex.split("npm stop"), cwd = ronoco_path + "/ronoco-blockly/")
